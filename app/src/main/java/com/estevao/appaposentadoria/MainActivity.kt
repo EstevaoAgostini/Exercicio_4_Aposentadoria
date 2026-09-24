57b84e13f062d3ec8bf72807f2c27c473f298bde
Estevão Nunes Agostini <estevaoagostini@gmail.com>
2026-09-24 14:30:58 -0300

    Adicionando componentes da tela

--- app/src/main/java/com/estevao/appaposentadoria/MainActivity.kt
+++ app/src/main/java/com/estevao/appaposentadoria/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.estevao.appaposentadoria
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.estevao.appaposentadoria.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Dados do Spinner:
+        val genero = listOf("Masculino", "Feminino")
+
+        //Config do Adapter
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, genero)
+
+        //Associa o adapter no spinner
+        binding.escolha.adapter = adapter
     }
 }
